628d54d409821bb99904ebd390645a0047e05a4f
Cuschieri Isaac <cuschierii@centralbankmalta.org>
2025-03-26 13:07:05 +0100

    revert to before plotly heatmap

--- heatmap_app/urls.py
+++ heatmap_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from . import views
+from . import views  # Import views from the heatmap_app
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('heatmap_image/', views.heatmap_image, name='heatmap_image'),
-    path('heatmap_image_plotly/', views.heatmap_image_plotly, name='heatmap_image_plotly'),  # ✅ New Plotly view
     path('detail/', views.detail, name='detail'),
     path('historical_plot_image/', views.historical_plot_image, name='historical_plot_image'),
     path('line_plot_image/', views.line_plot_image, name='line_plot_image'),
